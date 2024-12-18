5d41d9a53804cfe9ae962499207a1b68ae4514c0
Ngo Viet Thanh <ngovietthanh2701@gmail.com>
2024-12-18 19:29:57 +0700

    push code 18/12

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,294 @@
+/*
+* @copyright 2024 NGO VIET THANH
+*/
+
+/*
+* Fetch data to HTML
+*/
+
+document.addEventListener("DOMContentLoaded", () => {
+   const data = [
+      {
+         title: "2024 년 다낭 밤문화가 더욱 특별해지는 이유 titlePage",
+         image: "./../assets/images/banner.jpg",
+         textContact: "유흥을 원하여 접근을 하는",
+         navigationID: 1,
+         contents: [
+            {
+               title: "다낭마사지를 이용해야 하는 이유 - content 1 ",
+               image: "./assets/images/h1.jpg",
+               description: "content 1 다낭이라는 곳을 처음 접근하시는 분들은 대부분 골프여행 혹은 패키지 여행을 통해서 처음 접하게 되는 곳이기도 합니다. 하지만 기본적으로 유흥을 원하여 접근을 하는 것이 아니라서 시간이 많이 나지 않는 경우가 많이 있고, 짧은 시간이라도 제대로 즐기기 위해서 다낭마사지 업체를 잘 골라서 이용하시는 것이 가장 다낭마사지를 잘 이용할 수 있는 현명한 방법이라고 생각을 합니다. 그래서 오늘은 무엇보다 여러분들이 다낭마사지를 왜 이용을 해야하고 어떤 방식으로 이용을 하시는지 좋은것에 대해서 한번 로컬 업체들을 기준으로 하여 알아보도록 하겠습니다.",
+               sub_contents: [
+                  {
+                     title: "다낭마사지를 이용해야 하는 이유 subcontent",
+                     list: [
+                        {
+                           image: "./assets/images/h1.jpg",
+                           description: "sub_content 1 다낭이라는 곳을 처음 접근하시는 분들은 대부분 골프여행 혹은 패키지 여행을 통해서 처음 접하게 되는 곳이기도 합니다. 하지만 기본적으로 유흥을 원하여 접근을 하는 것이 아니라서 시간이 많이 나지 않는 경우가 많이 있고, 짧은 시간이라도 제대로 즐기기 위해서 다낭마사지 업체를 잘 골라서 이용하시는 것이 가장 다낭마사지를 잘 이용할 수 있는 현명한 방법이라고 생각을 합니다. 그래서 오늘은 무엇보다 여러분들이 다낭마사지를 왜 이용을 해야하고 어떤 방식으로 이용을 하시는지 좋은것에 대해서 한번 로컬 업체들을 기준으로 하여 알아보도록 하겠습니다.",
+                        },
+                        {
+                           image: "./assets/images/h2.jpg",
+                           description: "sub_content 1 다낭이라는 곳을 처음 접근하시는 분들은 대부분 골프여행 혹은 패키지 여행을 통해서 처음 접하게 되는 곳이기도 합니다. 하지만 기본적으로 유흥을 원하여 접근을 하는 것이 아니라서 시간이 많이 나지 않는 경우가 많이 있고, 짧은 시간이라도 제대로 즐기기 위해서 다낭마사지 업체를 잘 골라서 이용하시는 것이 가장 다낭마사지를 잘 이용할 수 있는 현명한 방법이라고 생각을 합니다. 그래서 오늘은 무엇보다 여러분들이 다낭마사지를 왜 이용을 해야하고 어떤 방식으로 이용을 하시는지 좋은것에 대해서 한번 로컬 업체들을 기준으로 하여 알아보도록 하겠습니다.",
+                        }
+                     ]
+                  },
+                  {
+                     title: "다낭마사지를 이용해야 하는 이유 subcontent",
+                     list: [
+                        {
+                           image: "./assets/images/h1.jpg",
+                           description: "sub_content 1 다낭이라는 곳을 처음 접근하시는 분들은 대부분 골프여행 혹은 패키지 여행을 통해서 처음 접하게 되는 곳이기도 합니다. 하지만 기본적으로 유흥을 원하여 접근을 하는 것이 아니라서 시간이 많이 나지 않는 경우가 많이 있고, 짧은 시간이라도 제대로 즐기기 위해서 다낭마사지 업체를 잘 골라서 이용하시는 것이 가장 다낭마사지를 잘 이용할 수 있는 현명한 방법이라고 생각을 합니다. 그래서 오늘은 무엇보다 여러분들이 다낭마사지를 왜 이용을 해야하고 어떤 방식으로 이용을 하시는지 좋은것에 대해서 한번 로컬 업체들을 기준으로 하여 알아보도록 하겠습니다.",
+                        },
+                        {
+                           image: "./assets/images/h2.jpg",
+                           description: "sub_content 1 다낭이라는 곳을 처음 접근하시는 분들은 대부분 골프여행 혹은 패키지 여행을 통해서 처음 접하게 되는 곳이기도 합니다. 하지만 기본적으로 유흥을 원하여 접근을 하는 것이 아니라서 시간이 많이 나지 않는 경우가 많이 있고, 짧은 시간이라도 제대로 즐기기 위해서 다낭마사지 업체를 잘 골라서 이용하시는 것이 가장 다낭마사지를 잘 이용할 수 있는 현명한 방법이라고 생각을 합니다. 그래서 오늘은 무엇보다 여러분들이 다낭마사지를 왜 이용을 해야하고 어떤 방식으로 이용을 하시는지 좋은것에 대해서 한번 로컬 업체들을 기준으로 하여 알아보도록 하겠습니다.",
+                        }
+                     ]
+                  },
+               ]
+            },
+            {
+               title: "다낭마사지를 이용해야 하는 이유 content 2 222",
+               image: "./assets/images/h1.jpg",
+               description: "2 다낭이라는 곳을 처음 접근하시는 분들은 대부분 골프여행 혹은 패키지 여행을 통해서 처음 접하게 되는 곳이기도 합니다. 하지만 기본적으로 유흥을 원하여 접근을 하는 것이 아니라서 시간이 많이 나지 않는 경우가 많이 있고, 짧은 시간이라도 제대로 즐기기 위해서 다낭마사지 업체를 잘 골라서 이용하시는 것이 가장 다낭마사지를 잘 이용할 수 있는 현명한 방법이라고 생각을 합니다. 그래서 오늘은 무엇보다 여러분들이 다낭마사지를 왜 이용을 해야하고 어떤 방식으로 이용을 하시는지 좋은것에 대해서 한번 로컬 업체들을 기준으로 하여 알아보도록 하겠습니다.",
+               sub_contents: [
+                  {
+                     title: "다낭마사지를 이용해야 하는 이유 subcontent",
+                     list: [
+                        {
+                           image: "./assets/images/h1.jpg",
+                           description: "sub_content 1 다낭이라는 곳을 처음 접근하시는 분들은 대부분 골프여행 혹은 패키지 여행을 통해서 처음 접하게 되는 곳이기도 합니다. 하지만 기본적으로 유흥을 원하여 접근을 하는 것이 아니라서 시간이 많이 나지 않는 경우가 많이 있고, 짧은 시간이라도 제대로 즐기기 위해서 다낭마사지 업체를 잘 골라서 이용하시는 것이 가장 다낭마사지를 잘 이용할 수 있는 현명한 방법이라고 생각을 합니다. 그래서 오늘은 무엇보다 여러분들이 다낭마사지를 왜 이용을 해야하고 어떤 방식으로 이용을 하시는지 좋은것에 대해서 한번 로컬 업체들을 기준으로 하여 알아보도록 하겠습니다.",
+                        },
+                        {
+                           image: "./assets/images/h2.jpg",
+                           description: "sub_content 1 다낭이라는 곳을 처음 접근하시는 분들은 대부분 골프여행 혹은 패키지 여행을 통해서 처음 접하게 되는 곳이기도 합니다. 하지만 기본적으로 유흥을 원하여 접근을 하는 것이 아니라서 시간이 많이 나지 않는 경우가 많이 있고, 짧은 시간이라도 제대로 즐기기 위해서 다낭마사지 업체를 잘 골라서 이용하시는 것이 가장 다낭마사지를 잘 이용할 수 있는 현명한 방법이라고 생각을 합니다. 그래서 오늘은 무엇보다 여러분들이 다낭마사지를 왜 이용을 해야하고 어떤 방식으로 이용을 하시는지 좋은것에 대해서 한번 로컬 업체들을 기준으로 하여 알아보도록 하겠습니다.",
+                        }
+                     ]
+                  },
+                  {
+                     title: "다낭마사지를 이용해야 하는 이유 subcontent",
+                     list: [
+                        {
+                           image: "./assets/images/h1.jpg",
+                           description: "sub_content 1 다낭이라는 곳을 처음 접근하시는 분들은 대부분 골프여행 혹은 패키지 여행을 통해서 처음 접하게 되는 곳이기도 합니다. 하지만 기본적으로 유흥을 원하여 접근을 하는 것이 아니라서 시간이 많이 나지 않는 경우가 많이 있고, 짧은 시간이라도 제대로 즐기기 위해서 다낭마사지 업체를 잘 골라서 이용하시는 것이 가장 다낭마사지를 잘 이용할 수 있는 현명한 방법이라고 생각을 합니다. 그래서 오늘은 무엇보다 여러분들이 다낭마사지를 왜 이용을 해야하고 어떤 방식으로 이용을 하시는지 좋은것에 대해서 한번 로컬 업체들을 기준으로 하여 알아보도록 하겠습니다.",
+                        },
+                        {
+                           image: "./assets/images/h2.jpg",
+                           description: "sub_content 1 다낭이라는 곳을 처음 접근하시는 분들은 대부분 골프여행 혹은 패키지 여행을 통해서 처음 접하게 되는 곳이기도 합니다. 하지만 기본적으로 유흥을 원하여 접근을 하는 것이 아니라서 시간이 많이 나지 않는 경우가 많이 있고, 짧은 시간이라도 제대로 즐기기 위해서 다낭마사지 업체를 잘 골라서 이용하시는 것이 가장 다낭마사지를 잘 이용할 수 있는 현명한 방법이라고 생각을 합니다. 그래서 오늘은 무엇보다 여러분들이 다낭마사지를 왜 이용을 해야하고 어떤 방식으로 이용을 하시는지 좋은것에 대해서 한번 로컬 업체들을 기준으로 하여 알아보도록 하겠습니다.",
+                        }
+                     ]
+                  },
+               ]
+            }
+         ]
+      },
+   ]
+
+   /* Add title table */
+
+   function fetchDataTable(data) {
+      const table_list = document.getElementById("table-list");
+
+      data[0].contents.forEach((item) => {
+         const li = document.createElement("li");
+         const a = document.createElement("a");
+         a.className = "title";
+         a.setAttribute("data-title-tabel", "");
+         a.href = "#";
+         a.textContent = item.title;
+         li.appendChild(a);
+
+         const sub_table_list = document.createElement("ol");
+         sub_table_list.className = "sub-list";
+
+         item.sub_contents.forEach((subItem) => {
+            const subLi = document.createElement("li");
+            const subA = document.createElement("a");
+            subA.className = "title";
+            subA.setAttribute("data-title-tabel", "");
+            subA.href = "";
+            subA.textContent = subItem.title;
+            subLi.appendChild(subA);
+            sub_table_list.appendChild(subLi);
+         });
+
+         li.appendChild(sub_table_list);
+
+         table_list.appendChild(li);
+      });
+   }
+
+   fetchDataTable(data);
+
+   /* Add content page */
+
+   function fetchDataContent(data) {
+      const contentList = document.getElementById("content-list");
+
+      data.forEach((item) => {
+         const mainLi = document.createElement("li");
+
+         const titleDiv = document.createElement("div");
+         titleDiv.className = 'title';
+
+         const iconDiv = document.createElement("div");
+         iconDiv.className = 'box-icon';
+         iconDiv.innerHTML = '<ion-icon name="help-outline"></ion-icon>';
+
+         const h2 = document.createElement("h2");
+         h2.textContent = item.title;
+         h2.setAttribute("data-title-content", "");
+
+         titleDiv.appendChild(iconDiv);
+         titleDiv.appendChild(h2);
+
+         /* add text */
+         const text = document.createElement('p');
+         text.className = 'text';
+         text.textContent = item.description;
+
+         /* add image */
+         const imgDiv = document.createElement("div");
+         imgDiv.className = 'box-image';
+         const img = document.createElement("img");
+         img.src = item.image;
+         img.alt = "Image for content";
+
+         imgDiv.appendChild(img);
+
+         mainLi.appendChild(titleDiv);
+         mainLi.appendChild(text);
+         mainLi.appendChild(imgDiv);
+
+         if (item.sub_contents && item.sub_contents.length > 0) {
+            const subUl = document.createElement("ul");
+
+            item.sub_contents.forEach((subItem) => {
+               const subLi = document.createElement("li");
+
+               const subTitleDiv = document.createElement("div");
+               subTitleDiv.className = "title";
+
+               const subIconDiv = document.createElement("div");
+               subIconDiv.className = "box-icon";
+               subIconDiv.innerHTML = '<ion-icon name="checkmark-outline"></ion-icon>';
+
+               const subH2 = document.createElement("h2");
+               subH2.textContent = subItem.title;
+               subH2.setAttribute("data-title-content", "");
+
+               subTitleDiv.appendChild(subIconDiv);
+               subTitleDiv.appendChild(subH2);
+
+               // Gắn mục con vào danh sách con
+               subLi.appendChild(subTitleDiv);
+
+               subItem.list.forEach(list => {
+                  const subText = document.createElement("p");
+                  subText.className = "text";
+                  subText.textContent = list.description;
+
+                  const subImageDiv = document.createElement("div");
+                  subImageDiv.className = "box-image";
+
+                  const subImg = document.createElement("img");
+                  subImg.src = list.image;
+                  subImg.alt = "Sub item image";
+
+                  subImageDiv.appendChild(subImg);
+                  subLi.appendChild(subText);
+                  subLi.appendChild(subImageDiv);
+
+               })
+               subUl.appendChild(subLi);
+            })
+
+            mainLi.appendChild(subUl);
+         }
+
+         contentList.appendChild(mainLi)
+
+      });
+   }
+
+   fetchDataContent(data[0].contents);
+
+});
+
+// function renderRichText(richText) {
+//    if (!Array.isArray(richText)) return "";
+
+//    return richText
+//       .map((block) => {
+//          if (block.type === "heading") {
+//             const level = block.level || 1;
+//             return `<h${level}>${renderChildren(block.children)}</h${level}>`;
+//          }
+//          if (block.type === "paragraph") {
+//             return `<p>${renderChildren(block.children)}</p>`;
+//          }
+//          return "";
+//       })
+//       .join("");
+// }
+
+// function renderChildren(children) {
+//    if (!Array.isArray(children)) return "";
+
+//    return children
+//       .map((child) => {
+//          if (child.type === "text") {
+//             let text = child.text;
+//             if (child.bold) text = `<strong>${text}</strong>`;
+//             if (child.underline) text = `<u>${text}</u>`;
+//             return text;
+//          }
+//          return "";
+//       })
+//       .join("");
+// }
+
+// function safeHTML(html) {
+//    return DOMPurify.sanitize(html);
+// }
+
+// const description = [
+//    {
+//       type: "heading",
+//       children: [
+//          {
+//             type: "text",
+//             text: "hello word, my name ngo viet thanh",
+//             bold: true,
+//             underline: true,
+//          },
+//       ],
+//       level: 4,
+//    },
+//    {
+//       type: "paragraph",
+//       children: [
+//          {
+//             type: "text",
+//             text: "hello word, my name ngo viet thanh",
+//             bold: true,
+//             underline: true,
+//          },
+//       ],
+//    },
+// ];
+
+// const richTextHTML = safeHTML(renderRichText(description));
+// document.getElementById("content").innerHTML = richTextHTML;
+
+
+/* 
+* Script layout 
+*/
 
 document.addEventListener("DOMContentLoaded", function () {
 
-   /*
-   * Click navbar
-   */
+   /* Click navbar */
    const navLinks = document.querySelectorAll("[data-nav-link]");
 
    if (navLinks.length > 0) {
@@ -17,10 +302,6 @@ document.addEventListener("DOMContentLoaded", function () {
       })
    });
 
-   /* 
-   *  Navbar
-   */
-
    const navbarList = document.querySelector("[data-navbar-list]");
    const navOpenBtn = document.querySelector("[data-nav-open-btn]");
 
@@ -31,26 +312,26 @@ document.addEventListener("DOMContentLoaded", function () {
 
    let lastScrollPosition = 0; // Lưu vị trí cuộn cuối cùng
 
-   // get height of li item
+   // Lấy chiều cao 1 hàng của phần tử navigate
    function getSingleRowHeight() {
-      const firstItem = navbarList.querySelector(".navbar-list .navbar-link.active");
-      return firstItem ? firstItem.offsetHeight : 0;
+      return navLinks[0].offsetHeight;
    }
 
-   // check navbar is overflow
+   // Check navbar is overflow
    function checkOverflow() {
       let totalItemWidth = 0;
 
       // caculator length toltal of all li
       const items = navbarList.querySelectorAll("[data-nav-link]");
       items.forEach(item => {
-         totalItemWidth += item.offsetWidth;
+         totalItemWidth += item.getBoundingClientRect().width;
       });
 
-      const listWidth = navbarList.clientWidth - (items.length - 1) * 7; // - gap each li
+      // caculator length(px) of navigate
+      const listWidth = navbarList.getBoundingClientRect().width - (items.length - 1) * 7; // - gap each li 7px 
 
       // compare width of total item li vs width of list
-      if (totalItemWidth > listWidth) { // if overflow width
+      if (Math.round(totalItemWidth) > Math.round(listWidth)) { // if overflow width
          navOpenBtn.style.display = 'block';
          navbarList.classList.add("collapsed");
          navbarList.style.maxHeight = getSingleRowHeight() + "px"; // set max-heigth = heigth of 1 row
@@ -64,7 +345,10 @@ document.addEventListener("DOMContentLoaded", function () {
    checkOverflow();
 
    // check dimension when resize
-   window.addEventListener('resize', checkOverflow);
+   window.addEventListener('resize', () => {
+      checkOverflow();
+      icon.setAttribute('name', 'chevron-down-outline');
+   });
 
    navOpenBtn.addEventListener("click", function () {
       // when click, list is collapsed, it is will expanded
@@ -95,17 +379,12 @@ document.addEventListener("DOMContentLoaded", function () {
             icon.setAttribute('name', 'chevron-down-outline');
          }
       }
-
       lastScrollPosition = currentScrollPosition;
    });
 
-
-   /*
-   *  GO TO TOP
-   */
-
+   /* GO TO TOP */
    const goTop = document.querySelector("[data-go-top]");
-   const header = document.querySelector("[data-header]");
+   const header = document.getElementById("header");
 
    window.addEventListener("scroll", () => {
       if (window.scrollY >= 140) {
@@ -114,19 +393,14 @@ document.addEventListener("DOMContentLoaded", function () {
          goTop.classList.remove("active");
       }
    });
-
    goTop.addEventListener('click', (e) => {
       e.preventDefault();
       smoothScrollTo(0, 750);
    });
 
-   /*
-   *  GO TABEL CONTENT
-   */
-
+   /* GO TABEL CONTENT */
    document.querySelectorAll("[data-title-content]").forEach(titleContent => {
       const uniqueId = generateUniqueId(titleContent.textContent);
-      console.log(uniqueId);
       titleContent.setAttribute('id', uniqueId);
    });
 
@@ -134,7 +408,6 @@ document.addEventListener("DOMContentLoaded", function () {
       titleTabel.addEventListener('click', function (e) {
          e.preventDefault();
          const targetId = generateUniqueId(this.textContent);
-         console.log(targetId);
          const targetElement = document.getElementById(targetId);
 
          if (targetElement) {
@@ -187,6 +460,3 @@ function easeInOutCubic(t) {
       4 * t * t * t :
       1 - Math.pow(-2 * t + 2, 3) / 2;
 }
-
-
-
